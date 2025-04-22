96814811ad916d17008a7b2f84f3fa895f39213e
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-23 00:38:44 +0700

    style: :art: formatting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,37 +1,31 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace SanSanLabs\MicrosoftGraph\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use SanSanLabs\MicrosoftGraph\MicrosoftGraphServiceProvider;
 
-class TestCase extends Orchestra
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
+class TestCase extends Orchestra {
+  protected function setUp(): void {
+    parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
+    Factory::guessFactoryNamesUsing(
+      fn(string $modelName) => "SanSanLabs\\MicrosoftGraph\\Database\\Factories\\" . class_basename($modelName) . "Factory",
+    );
+  }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            SkeletonServiceProvider::class,
-        ];
-    }
+  protected function getPackageProviders($app) {
+    return [MicrosoftGraphServiceProvider::class];
+  }
 
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
+  public function getEnvironmentSetUp($app) {
+    config()->set("database.default", "testing");
 
-        /*
+    /*
          foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
             (include $migration->getRealPath())->up();
          }
          */
-    }
+  }
 }
