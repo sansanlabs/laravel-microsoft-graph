7a1a19efceaff9b6158ba566de5205c0f902ef03
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-29 15:36:52 +0700

    refactor: :fire: remove default header and timeout

--- config/microsoftgraph.php
+++ config/microsoftgraph.php
@@ -7,20 +7,7 @@ return [
   "base_url" => "https://graph.microsoft.com/v1.0/",
 
   /*
-   * Default headers for all requests
-   */
-  "default_headers" => [
-    "Content-Type" => "application/json",
-    "Accept" => "application/json",
-  ],
-
-  /*
    * Session key for access token
    */
   "session_key" => "microsoft_access_token",
-
-  /*
-   * Default timeout for requests in seconds
-   */
-  "timeout" => 30,
 ];
